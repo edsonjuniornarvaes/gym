7e9573a5bc630900901e35ec5d01e63d7a5a5c4a
edsonjuniornarvaes <edsonjunior.narvaes@gmail.com>
2023-01-11 11:57:43 -0300

    feat: get token

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,8 @@ import axios, { AxiosInstance } from "axios";
 
 import { AppError } from "@utils/AppError";
 
+import { storageAuthTokenGet } from "@storage/storageAuthToken";
+
 type SignOut = () => void;
 
 type APIInstanceProps = AxiosInstance & {
@@ -9,18 +11,24 @@ type APIInstanceProps = AxiosInstance & {
 };
 
 const api = axios.create({
-  baseURL: "http://192.168.3.2:3333",
+  baseURL: "http://localhost:3333",
 }) as APIInstanceProps;
 
 api.registerInterceptTokenManager = (singOut) => {
   const interceptTokenManager = api.interceptors.response.use(
     (response) => response,
-    (requestError) => {
+    async (requestError) => {
       if (requestError.response?.status === 401) {
         if (
           requestError.response.data?.message === "token.expired" ||
           requestError.response.data?.message === "token.invalid"
         ) {
+          const oldToken = await storageAuthTokenGet();
+
+          if (!oldToken) {
+            singOut();
+            return Promise.reject(requestError);
+          }
         }
 
         singOut();

2e00058b59a54b7384725dd974c3939145db19af
edsonjuniornarvaes <edsonjunior.narvaes@gmail.com>
2023-01-11 11:45:18 -0300

    feat: api token

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -111,6 +111,14 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     loadUserData();
   }, []);
 
+  useEffect(() => {
+    const subscribe = api.registerInterceptTokenManager(signOut);
+
+    return () => {
+      subscribe();
+    };
+  }, []);
+
   return (
     <AuthContext.Provider
       value={{
